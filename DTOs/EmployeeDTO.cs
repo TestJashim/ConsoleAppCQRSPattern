e6643339475dce98ac6bbce68d6b2e7ed7b70016
MD. Jashim Uddin <ju.cu.acc@outlook.com>
2023-07-12 06:35:35 +0600

    Model and DTO

--- DTOs/EmployeeDTO.cs
+++ DTOs/EmployeeDTO.cs
@@ -2,5 +2,25 @@
 {
     public class EmployeeDTO
     {
+        public int Id
+        {
+            get;
+            set;
+        }
+        public string FullName
+        {
+            get;
+            set;
+        }
+        public int Age
+        {
+            get;
+            set;
+        }
+        public string Address
+        {
+            get;
+            set;
+        }
     }
 }
